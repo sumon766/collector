39000e0d7638dd1c64de74a40dd5107ab6cd959e
MD Mursedul Islam Sumon <sumon766@gmail.com>
2024-05-19 17:27:38 +0600

    Devise installed, signin up successfully and loggin in as well

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    configure_sign_up_params
+    super
+  end
 
   # GET /resource/edit
   # def edit
