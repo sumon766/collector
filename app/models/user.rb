15b13200d4ad6795ab9d357270653ecea23f6f54
MD Mursedul Islam Sumon <sumon766@gmail.com>
2024-05-19 21:44:06 +0600

    Remove cancancan and install pundit & setup gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
+  #Pundit role assignment
+  enum role: {user: 0, admin: 1}
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
 
